b2f13ac2d21ce3c5e670ffe334ac5ea00d07fb7b
osamamouatamid <oussamamouatamid1@gmail.com>
2025-04-21 12:24:14 +0100

    final lesson

---
+++ Ecommerce.BLL/Services/LessonService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ecommerce.DAL.Repositories;
+using Ecommerce.Models.DTOs;
+using Ecommerce.Models.Entities;
+
+namespace Ecommerce.BLL.Services
+{
+    public class LessonService
+    {
+
+        private readonly LessonRepository _repo;
+
+
+        public LessonService(LessonRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public List<LessonDto> GetAllLessons() => _repo.GetAllLessons();
+        public Lesson GetLessonId(int id) => _repo.GetLessonId(id);
+        public Lesson AddLesson(createLessonDto dto) => _repo.AddLesson(dto);
+
+        public bool DeleteLesson(int id) => _repo.DeleteLesson(id);
+        public Lesson UpdateLesson(int id, updateLessonDto l) => _repo.UpdateLesson(id, l);
+    }
+}
--- Ecommerce.DAL/Db/AppDbContext.cs
+++ Ecommerce.DAL/Db/AppDbContext.cs
@@ -17,6 +17,7 @@ namespace Ecommerce.DAL.Db
         public DbSet<Parent> Parents { get; set; }
 
         public DbSet<Student> Students { get; set; }
+        public DbSet<Lesson> Lesson { get; set; }
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Course> Courses { get; set; }

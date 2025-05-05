0f1fb08f76362c0f3089f7f4d5e43d2e340cc8d5
younes <younessaoinam22@gmail.com>
2025-05-05 11:02:30 +0200

    save my change before pull develop

--- Ecommerce.DAL/Migrations/20250430074939_init.Designer.cs
+++ Ecommerce.DAL/Migrations/20250430074939_init.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Ecommerce.DAL.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20250429084228_init")]
+    [Migration("20250430074939_init")]
     partial class init
     {
         /// <inheritdoc />
